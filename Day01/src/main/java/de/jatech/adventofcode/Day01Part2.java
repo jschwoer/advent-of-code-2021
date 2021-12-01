0e7ed906c8017745e75d5f3d8844b05ce95406f9
Jochen Schwoerer <9137747+jschwoer@users.noreply.github.com>
2021-12-01 15:07:59 +0100

    Solution for day 01 part 1 and part 2

---
+++ Day01/src/main/java/de/jatech/adventofcode/Day01Part2.java
@@ -0,0 +1,33 @@
+package de.jatech.adventofcode;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Day01Part2 {
+	public static void main(final String[] args) throws IOException, URISyntaxException {
+		final List<Integer> input = readList(Day01Part2.class.getClassLoader().getResource("input.txt"));
+
+		for (final int i : input) {
+			System.out.println(i);
+		}
+
+		int count = 0;
+		for (int idx = 1; idx < input.size(); idx++) {
+			if (input.get(idx) > input.get(idx - 1)) {
+				count++;
+			}
+		}
+
+		System.out.println("Counter: " + count);
+	}
+
+	private static List<Integer> readList(final URL url) throws IOException, URISyntaxException {
+		final List<String> allLines = Files.readAllLines(Paths.get(url.toURI()));
+		return allLines.stream().map(Integer::parseInt).collect(Collectors.toList());
+	}
+}
