88ff23ab001f1883e93f54dec31fab2ce8a57774
Jochen Schwoerer <9137747+jschwoer@users.noreply.github.com>
2021-12-04 05:43:00 +0100

    Prepare day 04

---
+++ src/main/java/de/jatech/adventofcode/day04/Day04Part2.java
@@ -0,0 +1,21 @@
+package de.jatech.adventofcode.day04;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import de.jatech.adventofcode.common.Utils;
+
+public class Day04Part2 {
+	public static void main(final String[] args) throws IOException, URISyntaxException {
+		final List<String> input = Utils.readLinesFromFile("day04/input.txt");
+
+		final int result = solvePuzzle(input);
+
+		System.out.println("Result: " + result);
+	}
+
+	static int solvePuzzle(final List<String> input) {
+		return 0;
+	}
+}
